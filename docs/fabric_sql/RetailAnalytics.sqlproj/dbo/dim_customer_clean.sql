8e3bf94c73d4c4fc327a07e95ff6df01ce02f533
Rohit Hasan <66373707+rohithasan@users.noreply.github.com>
2025-12-26 02:06:06 +0530

    Add files via upload

---
+++ docs/fabric_sql/RetailAnalytics.sqlproj/dbo/dim_customer_clean.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[dim_customer_clean] (
+
+	[customer_id] varchar(8000) NULL, 
+	[customer_name] varchar(8000) NULL, 
+	[Region] varchar(8000) NULL, 
+	[Segment] varchar(8000) NULL
+);
